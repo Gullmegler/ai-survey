4f7bca9918354c233a80dc55b8f5e93886d5961e
Softec Softwares <frankkarlseng@gmail.com>
2025-06-25 14:41:06 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,2 +1,16 @@
-import React from 'react';
-export default function App() { return <h1>AI Survey App</h1>; }
+import React from "react";
+import Navbar from "./components/Navbar";
+import LandingPage from "./components/LandingPage";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="font-sans text-gray-800">
+      <Navbar />
+      <LandingPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
