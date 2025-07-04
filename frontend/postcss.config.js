67a2840ab0d179752d588b9395ec3b3f0be2677e
Softec Softwares <frankkarlseng@gmail.com>
2025-07-04 11:41:18 +0100

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -4,7 +4,5 @@ echo "module.exports = {
     require('autoprefixer'),
   ],
 };" > postcss.config.js
-<<<<<<< HEAD
 
-=======
->>>>>>> 3bbcc39f22cf6f600ac9b14fa5c5de8935d6b63f
+ 3bbcc39f22cf6f600ac9b14fa5c5de8935d6b63f
